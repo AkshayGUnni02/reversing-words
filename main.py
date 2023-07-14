0a1641842c16def78908b2f26d6a540bf790a113
AkshayGUnni02 <98641910+AkshayGUnni02@users.noreply.github.com>
2023-07-14 11:42:20 +0530

    New code for reversing words

---
+++ main.py
@@ -0,0 +1,12 @@
+def reverse_words(input_string):
+    words = input_string.split()
+    if len(words) != 2:
+        return "Please provide exactly two words separated by a space."
+    else:
+        reversed_words = words[::-1]
+        return ' '.join(reversed_words)
+
+# Example usage
+input_str = input("Enter two words: ")
+reversed_str = reverse_words(input_str)
+print("Reversed words:", reversed_str)
